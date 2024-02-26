3d69ac648a487a96dccfab14cfea23db85f327db
drgmk <grantkennedy@gmail.com>
2024-02-26 19:19:38 +0000

    initial commits

---
+++ examples/hello_world.py
@@ -0,0 +1,128 @@
+# minimal example script
+import numpy as np
+from scipy.special import jn_zeros
+import matplotlib.pyplot as plt
+import multiprocess as mp
+import frank
+import emcee
+import vis_r
+
+# initial parameters
+params = ['dra', 'ddec', 'PA', 'inc', 'flux', 'r0', 'dr', 'dz']
+p0 = np.array([0.03, 0.03, 26.7, 76.7, 0.015, 1.08, 0.06, 0.04])
+
+# get visibilities
+uv_file = '../data/hr4796.selfcal.npy'
+u, v, Re, Im, w, wavelength, ms_file = np.load(uv_file, allow_pickle=True)
+
+# estimate re-weighting factor
+# so that chi^2 for null model would be 1, and d.o.f = 2*len(w)
+reweight_factor = 2*len(w) / np.sum((Re**2.0 + Im**2.0) * w)
+print('reweighting factor is {}'.format(reweight_factor))
+w *= reweight_factor
+
+# bin the data
+sz = 4
+u, v, Re, Im, w = vis_r.bin_uv(u, v, Re, Im, w, size_arcsec=sz)
+
+# set up the DHT
+arcsec = np.pi/180/3600
+arcsec2pi = arcsec*2*np.pi
+
+uvmax = np.max(np.sqrt(u**2 + v**2))
+uvmin = np.min(np.sqrt(u**2 + v**2))
+
+fac = 1.5  # safety factor
+r_max = jn_zeros(0, 1)[0] / (2*np.pi*uvmin*np.cos(np.deg2rad(p0[3]))) / arcsec * fac
+
+nhpt = 1
+while True:
+    q_tmp = jn_zeros(0, nhpt)[-1]
+    if q_tmp > uvmax * 2*np.pi*r_max*arcsec:
+        break
+    nhpt += 1
+
+h = frank.hankel.DiscreteHankelTransform(r_max*arcsec, nhpt)
+Rnk, Qnk = h.get_collocation_points(r_max*arcsec, nhpt)
+
+
+# radial profile
+def r_prof(r, par):
+    return np.exp(-0.5*np.square((r - par[5])/par[6]))
+
+
+def lnprob(p, test=False):
+
+    # u,v rotation
+    urot, ruv = vis_r.uv_trans(u, v, np.deg2rad(p[2]), np.deg2rad(p[3]))
+
+    # radial profile
+    f = 1/2.35e-11*r_prof(Rnk/arcsec, p)
+    fth = h.transform(f)
+
+    # normalise on shortest baseline
+    fth = fth * p[4]/fth[0]
+
+    # interpolate, frank has a method for this too
+    # but it is about 10x slower
+    # vis = h.interpolate(fth, ruv, space='Fourier')
+    vis = np.interp(ruv, Qnk, fth)
+
+    # vertical structure
+    rz = p[7]*p[5]*np.sin(np.deg2rad(p[3])) * urot * arcsec2pi
+    vis = vis * np.exp(-0.5*np.square(rz))
+
+    # phase shift
+    rot = (u*p[0] + v*p[1])*arcsec2pi
+    vis = vis * np.exp(1j*rot)
+
+    # chi^2
+    chi2 = np.sum(((Re-vis.real)**2.0 + (Im-vis.imag)**2.0) * w)
+    if test:
+        print(-0.5*chi2)
+        return ruv, fth, vis
+
+    return -0.5 * chi2
+
+
+# set up and run mcmc fitting
+ndim = len(p0)
+nwalkers = 18
+nsteps = 1000
+nthreads = 6
+
+# we are using emcee v3
+with mp.Pool(nthreads) as pool:
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, pool=pool)
+    pos = [p0 + p0*0.01*np.random.randn(ndim) for i in range(nwalkers)]
+    pos, prob, state = sampler.run_mcmc(pos, nsteps, progress=True)
+
+# see what the chains look like, skip a burn in period
+burn = nsteps - 200
+fig, ax = plt.subplots(ndim+1, 2, figsize=(9.5, 5), sharex='col', sharey=False)
+
+for j in range(nwalkers):
+    ax[-1, 0].plot(sampler.lnprobability[j, :burn])
+    for i in range(ndim):
+        ax[i, 0].plot(sampler.chain[j, :burn, i])
+        ax[i, 0].set_ylabel(params[i])
+
+for j in range(nwalkers):
+    ax[-1, 1].plot(sampler.lnprobability[j, burn:])
+    for i in range(ndim):
+        ax[i, 1].plot(sampler.chain[j, burn:, i])
+        # ax[i, 1].set_ylabel(params[i])
+
+ax[-1, 0].set_xlabel('burn in')
+ax[-1, 1].set_xlabel('sampling')
+fig.savefig('example-chains.png')
+
+p = np.median(sampler.chain[:, burn:, :].reshape((-1, ndim)), axis=0)
+ruv, fth, vis = lnprob(p, test=True)
+fig, ax = plt.subplots()
+ax.scatter(ruv/1e6, Re, s=0.1)
+ax.scatter(ruv/1e6, vis.real, s=0.1, color='yellow')
+ax.set_xlabel('baseline / M$\lambda$')
+ax.set_ylabel('flux / Jy')
+fig.tight_layout()
+fig.savefig('example-vis.png')
---
+++ src/vis_r/functions.py
@@ -0,0 +1,113 @@
+import numpy as np
+from scipy.stats import binned_statistic_2d
+
+
+def read_vis(f):
+    """Read visibilities in a few niche formats."""
+    if '.npy' in f:
+        # my format, u/v already in wavelengths
+        u, v, re, im, w, wavelength_, ms_file_ = np.load(f, allow_pickle=True)
+    elif '.txt' in f:
+        # 3 comment lines, line 2 is average wave in m
+        # then u[m], v[m], Re, Im, w (5 lines)
+        # or u[lambda], v[lambda], Re, Im, w, wavelength (6 lines)
+        tmp = np.loadtxt(f, comments='#')
+        if tmp.shape[1] == 5:
+            fh = open(f)
+            lines = fh.readlines()
+            wavelength_ = float(lines[1].strip().split(' ')[-1])
+            u, v, re, im, w = tmp.T
+            u /= wavelength_
+            v /= wavelength_
+        elif tmp.shape[1] == 6:
+            u, v, re, im, w, wavelength_ = tmp.T
+            # u /= wavelength_
+            # v /= wavelength_
+        else:
+            exit('text file has neither 5 nor 6 columns')
+    else:
+        exit('file type not txt or npy')
+
+    return u, v, re, im, w
+
+
+def _get_duv(R, wav, D):
+    """Equivalent to get_duv below"""
+    return 1/0.6 * D/wav * np.sqrt(1/R**2 - 1)
+
+
+def get_duv(r=0.99, size_arcsec=None):
+    """Return u,v cell size for binning.
+
+    Parameters
+    ----------
+    r: float, optional
+        Flux loss for point source at size_arcsec
+    size_arcsec: float, optional
+        How far from phase center pt. src. will lose (1-r)% flux.
+    """
+    if size_arcsec is None:
+        size_arcsec = 8.84
+    return 1/(size_arcsec/3600*np.pi/180) * np.sqrt(1/r**2 - 1)
+
+
+def bin_uv(u_, v_, re_, im_, w_, size_arcsec=None):
+    """Return binned visibilities.
+
+    Parameters
+    ----------
+    u_, v_, re_, im_, w_: arrays of floar
+        u,v baselines, real and imaginary visibilities, and weights.
+    size_arcsec: float, optional
+        Passed to get_duv to get bin size.
+    """
+
+    uneg = u_ < 0
+    u_ = np.abs(u_)
+    v_[uneg] = -v_[uneg]
+    im_[uneg] *= -1
+
+    binsz = get_duv(size_arcsec=size_arcsec)
+    print(f'uv bin: {binsz:.2f}')
+
+    bins = [int(np.max(np.abs(v_))/binsz), int(np.max(np.abs(u_))/binsz)*2]
+
+    u,  _, _, _ = binned_statistic_2d(u_, v_, u_*w_, statistic='sum', bins=bins)
+    v,  _, _, _ = binned_statistic_2d(u_, v_, v_*w_, statistic='sum', bins=bins)
+    re, _, _, _ = binned_statistic_2d(u_, v_, re_*w_, statistic='sum', bins=bins)
+    im, _, _, _ = binned_statistic_2d(u_, v_, im_*w_, statistic='sum', bins=bins)
+    w,  _, _, _ = binned_statistic_2d(u_, v_, w_, statistic='sum', bins=bins)
+
+    # keep non-empty cells
+    ok = w != 0
+    u = (u[ok] / w[ok]).flatten()
+    v = (v[ok] / w[ok]).flatten()
+    re = (re[ok] / w[ok]).flatten()
+    im = (im[ok] / w[ok]).flatten()
+    w = w[ok].flatten()
+
+    return u, v, re, im, w
+
+
+def uv_trans(u, v, PA, inc, return_uv=False):
+    """Return transformed and rotated u,v
+
+    Parameters
+    ----------
+    u, v: arrays of float
+        u,v baselines.
+    PA: float
+        Position angle in radians.
+    inc: float
+        Inclination in radians.
+    return_uv: bool, optional
+        Return rotated u,v.
+    """
+    cos_PA = np.cos(PA)
+    sin_PA = np.sin(PA)
+    urot = u * cos_PA - v * sin_PA
+    vrot = u * sin_PA + v * cos_PA
+    if return_uv:
+        return urot, vrot
+    ruv = np.hypot(urot*np.cos(inc), vrot)
+    return urot, ruv
